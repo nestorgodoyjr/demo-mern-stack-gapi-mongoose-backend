2a85ee2120600211a9ce83405ede26c282a1717c
[NestorJr Godoy] <[nestorgodoyjr@gmail.com]>
2024-08-16 18:55:41 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -38,9 +38,24 @@ const credentialsPath = 'credentials.json';
 // Function to authenticate and get Google Sheets client
 async function getSheetsClient() {
     const auth = new google.auth.GoogleAuth({
-        keyFile: credentialsPath,
+         keyFile: credentialsPath,
+        credentials:{
+    //         type: process.env.TYPE,
+    //         project_id: process.env.PROJECT_ID,
+    //         private_key_id: process.env.PRIVATE_KEY_ID,
+             private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+             client_email: process.env.CLIENT_EMAIL,
+    //         client_id: process.env.CLIENT_ID,
+    //         auth_uri: process.env.AUTH_URI,
+    //         token_uri: process.env.TOKEN_URI,
+    //         auth_provider_x509_cert_url: process.env.AUTH_PROVIDER,
+    //         client_x509_cert_url: process.env.CLIENT,
+    //         universe_domain: process.env.UNIVERSAL_DOMAIN,
+        },
         scopes: SCOPES,
     });
+
+    //console.log()
 
     const authClient = await auth.getClient();
     const sheets = google.sheets({ version: 'v4', auth: authClient });
